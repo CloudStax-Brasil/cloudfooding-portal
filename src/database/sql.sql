6d54b9ca7ad3b2972caee5ca5c1c9654cefef029
RafaBSilvaDev <RAFAEL.BSILVA@bandtec.com.br>
2022-04-20 21:02:36 -0300

    fix: alterações nas configurações da conexão com o banco

--- src/database/sql.sql
+++ src/database/sql.sql
@@ -1,21 +1,58 @@
-CREATE DATABASE fpstips;
-
-USE fpstips;
-
-CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50)
+CREATE TABLE empresa (
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+	nomeEmpresa VARCHAR(45),
+	cnpjEmpresa VARCHAR(45),
+	emailEmpresa VARCHAR(45),
+	senhaEmpresa VARCHAR(45)
 );
 
-select * from usuario;
-
-CREATE TABLE quiz (
-idQuiz INT PRIMARY KEY AUTO_INCREMENT,
-qtdAcertos INT,
-fk_usuario INT,
-FOREIGN KEY (fk_usuario) references usuario(id)
+CREATE TABLE gerente (
+	idGerente INT PRIMARY KEY AUTO_INCREMENT,
+	nomeGerente VARCHAR(45),
+	emailGerente VARCHAR(45),
+	senhaGerente VARCHAR(45),
+	fkEmpresa INT,
+	FOREIGN KEY (idEmpresa) REFERENCES (fkEmpresa)
 );
 
-select * from quiz;
+CREATE TABLE funcionario (
+	idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
+	nomeFuncionario VARCHAR(45),
+	emailFuncionario VARCHAR(45),
+	senhaFuncionario VARCHAR(45),
+	fkGerente INT,
+	FOREIGN KEY (idGerente) REFERENCES (fkGerente)
+);
+
+CREATE TABLE franquias(
+	idFranquias INT PRIMARY KEY AUTO_INCREMENT,
+	nomeFraquia VARCHAR(45),
+	local VARCHAR(45),
+	fkEmpresa INT,
+	FOREIGN KEY (idEmpresa) REFERENCES (fkEmpresa)
+
+);
+
+CREATE TABLE maquina (
+	idMaquina INT PRIMARY KEY AUTO_INCREMENT,
+	nomeMaquina VARCHAR(45),
+	fkFranquias INT,
+	FOREIGN KEY (idFranquias) REFERENCES (fkFranquias)
+);
+
+CREATE TABLE cpu (
+	idCPU INT PRIMARY KEY AUTO_INCREMENT,
+	temperatura INT,
+	uso INT,
+	hora DATETIME,
+	fkMaquina INT,
+	FOREIGN KEY (idMaquina) REFERENCES (fkMaquina)
+);
+
+CREATE TABLE memoriaRam (
+	idmemoriaRam INT PRIMARY KEY AUTO_INCREMENT,
+	uso INT,
+	horario DATETIME,
+	fkMaquina,
+	FOREIGN KEY (idMaquina) REFERENCES (fkMaquina)
+);
