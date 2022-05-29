a85c6fed3b5e53ed571fc1f975a2b930dfb9aa1c
guilherme.novais <guilherme.novais@valemobi.com.br>
2022-05-29 00:43:57 -0300

    42-feature-conexão-do-banco-com-o-chartjs

---
+++ site/src/controllers/dashboardController.js
@@ -0,0 +1,44 @@
+var dashModel = require("../models/dashboardModel")
+
+function buscarUltimasMedidasRam(req, res) {
+
+    const limite_linhas = 7;
+
+    var idFuncionario = req.params.idFuncionario;
+
+    console.log(`Recuperando as ultimas ${limite_linhas} medidas`);
+
+    medidaModel.buscarUltimasMedidasRam(idFuncionario, limite_linhas).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+function buscarMedidasEmTempoReal(req, res) {
+
+    var idFuncionario = req.params.idFuncionario;
+
+    console.log(`Recuperando medidas em tempo real`);
+
+    medidaModel.buscarMedidasEmTempoReal(idFuncionario).then(function (resultado) {
+        if (resultado.length > 0) {
+            res.status(200).json(resultado);
+        } else {
+            res.status(204).send("Nenhum resultado encontrado!")
+        }
+    }).catch(function (erro) {
+        console.log(erro);
+        console.log("Houve um erro ao buscar as ultimas medidas.", erro.sqlMessage);
+        res.status(500).json(erro.sqlMessage);
+    });
+}
+module.exports = {
+    buscarUltimasMedidasRam,
+    buscarMedidasEmTempoReal
+}
---
+++ site/src/routes/dashboard.js
@@ -0,0 +1,8 @@
+var express = require("express");
+var router = express.Router();
+
+var dashboardController = require("../controllers/dashboardController")
+
+router.get("/listarRam", function (req, res){
+    dashboardController.buscarUltimasMedidasRam(req, res);
+})
