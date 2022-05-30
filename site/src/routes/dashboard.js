63ef296f7a623676aa8fb1729db45309dd3f7755
guilherme.novais <guilherme.novais@valemobi.com.br>
2022-05-30 11:52:31 -0300

    Ferramenta de Suporte

--- site/src/routes/dashboard.js
+++ site/src/routes/dashboard.js
@@ -5,4 +5,7 @@ var dashboardController = require("../controllers/dashboardController")
 
 router.get("/listarRam", function (req, res){
     dashboardController.buscarUltimasMedidasRam(req, res);
-})  
+}); 
+router.get("/listarCP  ", function (req, res){
+    dashboardController.buscarUltimasMedidasCPU(req, res);
+}); 
