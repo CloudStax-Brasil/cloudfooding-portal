ebd43f0caf6c459eafd6b00d06754a1aa2bb31df
Gabriel Oliveira Aguiar <gabriel.osilva@bandtec.com.br>
2022-05-30 20:59:35 -0300

    resolvendo conflitos

--- site/src/models/usuarioModel.js
+++ site/src/models/usuarioModel.js
@@ -10,7 +10,7 @@ function listar() {
 }
 
 function listarCaixas(fkGerente) {
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarCaixas()");
+    // console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function listarCaixas()");
     var instrucao = `
         SELECT * FROM funcionario WHERE fkGerente = ${fkGerente};
     `;
