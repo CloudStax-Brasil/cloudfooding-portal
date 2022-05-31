cbfb6cee4288c4552f0fbaf958527233b9eb1fb0
Rafael Barbosa <rbsilva12@stefanini.com>
2022-05-31 01:27:22 -0300

    56-feature-integração-do-banco-com-o-gráfico

--- site/src/routes/dashboard.js
+++ site/src/routes/dashboard.js
@@ -4,12 +4,20 @@ var router = express.Router();
 
 var dashboardController = require("../controllers/dashboardController")
 
-router.post("/buscarUltimasMedidasCPU", function (req, res){
+router.post("/buscarUltimasMedidasCPU", function (req, res) {
     dashboardController.buscarUltimasMedidasCpu(req, res);
 });
 
-router.post("/buscarUltimasMedidasRam", function (req, res){
+router.post("/buscarUltimasMedidasRam", function (req, res) {
     dashboardController.buscarUltimasMedidasRam(req, res);
+})
+
+router.post("/buscarMedidasCPUTempoReal", function (req, res) {
+    dashboardController.buscarMedidasEmTempoRealCPU(req, res);
+});
+
+router.post("/buscarMedidasRamTempoReal", function (req, res) {
+    dashboardController.buscarMedidasEmTempoRealRAM(req, res);
 })
 
 module.exports = router;
